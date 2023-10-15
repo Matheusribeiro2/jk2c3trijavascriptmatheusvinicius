3d4fb988ea61ad7fe1fed8694eea5ef545eba77b
Matheusribeiro2 <110207936+Matheusribeiro2@users.noreply.github.com>
2023-10-14 23:38:36 -0300

    Aula 4

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,2 +1,11 @@
-alert('Olá mundo!');
-document.querySelector('input[type=tel]');
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+
+  const tecla = listaDeTeclas[indice];
+
+  tecla.onclick = function () {
+    inputTel.value = inputTel.value + tecla.value;
+  }
+}
