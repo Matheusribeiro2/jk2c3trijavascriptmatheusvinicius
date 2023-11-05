ec85cf6b23b4284aaa092a7d86c29ece7f82b3b2
Matheusribeiro2 <110207936+Matheusribeiro2@users.noreply.github.com>
2023-11-05 18:56:35 -0300

    aula 5 - projeto concluído

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,14 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
 
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+        
+    } else {
+        console.log('Elemento não encotrado ou seletor inválido');
+
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll(`.tecla`);
@@ -11,7 +19,20 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
+    
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    
+    tecla.onkeydown = function(evento) {
+        
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+           tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
     }
 }
